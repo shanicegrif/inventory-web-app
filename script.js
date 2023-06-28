f2f0b2a40311ad0979fc6acf4d08313fdfa7215c
Shanice Griffin <shanicegriffin@Shanices-MacBook-Air.local>
2023-06-27 21:15:19 -0400

    coffee template function near done

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
 const form = document.querySelector("form");
 
-form.addEventListener("sumit", (event) => {
+form.addEventListener("submit", (event) => {
     event.preventDefault();
+    let name = event.target.name.value;
+    let origin = event.target.origin.value;
+    let price = event.target.price.value;
+    let strength = event.target.strength.value;
+    let stock = event.target.stock.value;
 
+    console.log(name);
+    console.log(origin);
+    console.log(price);
+    console.log(strength);
+    console.log(stock)
 })
+
+function coffeeTempl(name, origin, price, strength, stock) {
+    const li = document.createElement("li");
+    li.textContent += name;
+
+    if (origin) {
+        const italic = document.createElement("em");
+        italic.textContent = "Origin: ";
+        li.append(document.createElement("br"), italic, origin);
+    }
+    
+    if (price) {
+        const strong = document.createElement("strong");
+        strong.textContent = "Price: ";
+        li.append(document.createElement("br"), strong, price);
+    }
+    
+    if (strength) {
+        const strong = document.createElement("strong");
+        strong.textContent = "Coffe Strength: ";
+        li.append(document.createElement("br"), strong, strength);
+    }
+    if (stock) {
+        const strong = document.createElement("strong");
+        strong.textContent = "Stock: ";
+        li.append(document.createElement("br"), strong, stock);
+    }
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete Coffee";
+    console.log(deleteButton);
+    li.append(deleteButton);
+
+    deleteButton.addEventListener("click", () => {
+        li.remove();
+    })
+
+    return li;
+}
